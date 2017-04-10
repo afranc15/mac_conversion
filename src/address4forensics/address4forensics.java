71c03db732a5c3c8015927e29d0310a07ab4d384
Anthon Taylor <anthonjtaylor@gmail.com>
2017-04-10 12:29:43 -0700

    layout of code

--- src/address4forensics/address4forensics.java
+++ src/address4forensics/address4forensics.java
@@ -8,7 +8,55 @@ public class address4forensics {
             "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"};
 
     public static void main(String[] args) {
-
-                System.out.println("Hi.");
+         if(args.length != 0){
+            String hex = getString(args);
+            if(args[0].equalsIgnoreCase("-L")){
+                //logical address
+            }else if(args[0].equalsIgnoreCase("-P")){
+                //physical address
+            }else if (args[0].equalsIgnoreCase("-P")){
+                //cluster address
+            }
+            else{
+                System.out.println("Error. Incorrect input.");
+            }
+        }
     }
+
+    private static String readFromFile(String file){
+        String hex;
+        try {
+            FileReader fileReader = new FileReader(file);
+            Scanner scanner =  new Scanner(fileReader);
+            hex = scanner.nextLine();
+            scanner.close();
+            return hex;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //get string
+
+    //logical address -L
+
+    //physical address -P
+
+    //cluster address -C
+
+    //---------------
+
+    //offset -b
+
+    //byte-address -B
+
+    //sector size -s (requires -B)
+
+    //address -l when -C or -P is used. Else returns logical address
+
+    //address -p when -C or -L are used. Else returns physical address
+
+    //address -c when -P or -L are used. Else returns cluster address. -k -r -t -f are required for this.
+
 }
