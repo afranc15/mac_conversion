b69724daf7849c4b5b090190fc448a5ff3701be0
Anthon Taylor <anthonjtaylor@gmail.com>
2017-04-13 22:33:47 -0700

    Imma take a diff route

--- src/address4forensics/address4forensics.java
+++ src/address4forensics/address4forensics.java
@@ -1,20 +1,23 @@
 import java.io.*;
 import java.util.*;
 
+
+//account for -B, --byte-address at some point
+
 public class address4forensics {
 
     public static void main(String[] args) {
          if(args.length != 0){
 
-            if(args[0].equalsIgnoreCase("-L")){
+             if(args[0].equalsIgnoreCase("-L") || args[0].equalsIgnoreCase("--logical")){
                 logicaladdress(args);
-            }else if(args[0].equalsIgnoreCase("-P")){
+             }else if(args[0].equalsIgnoreCase("-P") || args[0].equalsIgnoreCase("--physical")){
                 physicaladdress(args);
-            }else if (args[0].equalsIgnoreCase("-C")){
+             }else if (args[0].equalsIgnoreCase("-C") || args[0].equalsIgnoreCase("--cluster")){
                 clusteraddress(args);
             }
             else{
-                System.out.println("Your input is incorrect: " + Arrays.toString(args));
+                System.out.println("Your input is incorrect in main: " + Arrays.toString(args));
             }
         }
     }
@@ -28,20 +31,20 @@ public class address4forensics {
                 System.out.print(val[1] + " ");
                 System.out.print(val[2] + " ");
             } else if (val[1].contains("--partition-start")) {
-                //parseValue
+                //parseAssignmentVal
             } else {
                 //set -b to 0 otherwise
             }
 
             if (val[3].contains("--physical-known") || val[3].equals("-p")){
                 System.out.println(val[3]);
-                //calculate with physical-known
+
             } else if (val[3].contains("--cluster-known") ||  val[3].equals("-c")){
                 System.out.println(val[3]);
-                clusterknown(val[5], val[7], val[9], val[11]);
+                clusterknown(val[5], val[7], val[9], val[11], "logical");
             }
         } catch (Exception e) {
-            System.out.println("Your input is incorrect: " + Arrays.toString(val));
+            System.out.println("Your input is incorrect in logical address: " + Arrays.toString(val));
         }
 
     }
@@ -56,20 +59,20 @@ public class address4forensics {
                 System.out.print(val[1] + " ");
                 System.out.print(val[2] + " ");
             } else if (val[1].contains("--partition-start")) {
-                //parseValue
+                //parseAssignmentVal
             } else {
                 //set -b to 0 otherwise
             }
 
             if (val[3].contains("--logical-known") || val[3].equals("-l")){
                 System.out.println(val[3]);
-                //calculate with physical-known
+
             } else if (val[3].contains("--cluster-known") ||  val[3].equals("-c")){
                 System.out.println(val[3]);
-                clusterknown(val[5], val[7], val[9], val[11]);
+                clusterknown(val[5], val[7], val[9], val[11], "physical");
             }
         } catch (Exception e) {
-            System.out.println("Your input is incorrect: " + Arrays.toString(val));
+            System.out.println("Your input is incorrect in physical address: " + Arrays.toString(val));
         }
     }
 
@@ -82,32 +85,36 @@ public class address4forensics {
                 System.out.print(val[1] + " ");
                 System.out.print(val[2] + " ");
             } else if (val[1].contains("--partition-start")) {
-                //parseValue
+                //parseAssignmentVal
             } else {
                 //set -b to 0 otherwise
             }
 
             if (val[3].contains("--logical-known") || val[3].equals("-l")){
                 System.out.println(val[3]);
-                //calculate with physical-known
+
             } else if (val[3].contains("--physical-known") ||  val[3].equals("-p")){
                 System.out.println(val[3]);
-                clusterknown(val[5], val[7], val[9], val[11]);
+                clusterknown(val[5], val[7], val[9], val[11], "cluster");
             }
         } catch (Exception e) {
-            System.out.println("Your input is incorrect: " + Arrays.toString(val));
+            System.out.println("Your input is incorrect in cluster address: " + Arrays.toString(val));
         }
     }
 
-    private static void physicalknown(String val){
+    private static void physicalknown(String val, String addressType){
         //
     }
 
-    private static void logicalknown(String val){
+    private static void logicalknown(String val, String addressType){
         //
     }
 
-    private static void clusterknown(String k, String r, String t, String f){
+    private static void clusterknown(String k, String r, String t, String f, String addressType){
         System.out.println("k: " + k + " r: " + r + " t: " + t + " f: " + f);
+    }
+
+    private static String parseAssignment(String k){
+        return "";
     }
 }
