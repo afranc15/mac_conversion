8dddee8f7edbb67dbcc57a8c97f6b8478ec0d4ee
Anthon Taylor <anthonjtaylor@gmail.com>
2017-04-14 23:04:45 -0700

    project setup

---
+++ src/analysis/analysis.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+
+
+public class analysis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
