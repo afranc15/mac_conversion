61bd77a5002ca43d781b5d20f320cc1511da6415
afranc15 <afranc15@users.noreply.github.com>
2017-05-01 09:57:38 -0700

    Modify analysis
    Changed to take args from command line. Works with Windows! 
    Command for windows:
    javac -cp guava-21.0.jar analysis.java
    java -cp guava-21.0.jar;. analysis <ImageFile>

--- src/analysis/analysis.java
+++ src/analysis/analysis.java
@@ -16,95 +16,65 @@ public class analysis {
     private static ArrayList<Long> fatList = new ArrayList<Long>();
 
     public static void main(String[] args) {
-        try
-            {
-                //===== Get File and Hash Value =======//
+    	if(args.length != 0){
+	        try
+	            {
+	        	
+	                //===== Get File and Hash Value =======//
+	        		
+	
+	                File f = new File(args[0]);
+	
+	                HashCode sha = Files.hash(f , Hashing.sha1());
+	                HashCode md5 = Files.hash(f , Hashing.md5());
+	
+	                //===== Parse Name and Create File  =======//
+	
+	                String fName = f.getName();
+	
+	                fName = fName.substring(0, fName.lastIndexOf("."));
+	
+	                File md5File = new File("Md5-" + fName + ".txt");
+	                File sha1File = new File("Sha1-" + fName + ".txt");
+	                
+	                //===== Print Value =======//
+	            	
+	                System.out.println(fName + " MD5: " + md5.toString());
+	                System.out.println(fName + " SHA-1: " + sha.toString());
+	
+	                System.out.println("Image Name: " + f.getName());
+	
+	                md5File.createNewFile();
+	                sha1File.createNewFile();
+	
+	
+	                //===== Write Hashes to Files  =======//
+	
+	                FileOutputStream isa = new FileOutputStream(md5File);
+	                OutputStreamWriter oswa = new OutputStreamWriter(isa);
+	                Writer wa = new BufferedWriter(oswa);
+	                wa.write(md5.toString());
+	                wa.close();
+	
+	                FileOutputStream isb = new FileOutputStream(sha1File);
+	                OutputStreamWriter oswb = new OutputStreamWriter(isb);
+	                Writer wb = new BufferedWriter(oswb);
+	                wb.write(sha.toString());
+	                wb.close();
+	
+	
+	                //===== Start reading Files =====//
+	                System.out.println();
+	                byte[] bytes = Files.toByteArray(f);
+	                findAllPartitions(bytes);
+	                findAllFAT(bytes);
 
-                URL path = analysis.class.getResource("TestImage1.img");
-
-                File f = new File(path.getFile());
-
-                HashCode sha = Files.hash(f , Hashing.sha1());
-                HashCode md5 = Files.hash(f , Hashing.md5());
-
-                URL pathb = analysis.class.getResource("TestImage2.img");
-
-                File fb = new File(pathb.getFile());
-
-                HashCode shab = Files.hash(fb , Hashing.sha1());
-                HashCode md5b = Files.hash(fb , Hashing.md5());
-
-                //===== Print Value =======//
-
-                System.out.println("Test Image 1 MD5: " + md5.toString());
-                System.out.println("Test Image 1 SHA-1: " + sha.toString());
-
-                System.out.println("Test Image 2 MD5: " + md5b.toString());
-                System.out.println("Test Image 2 SHA-1: " + shab.toString());
-
-                System.out.println("Image Name: " + f.getName());
-                System.out.println("Image Name: " + fb.getName());
-
-                //===== Parse Name and Create File  =======//
-
-                String fName = f.getName();
-                String fbName = fb.getName();
-
-                fName = fName.substring(0, fName.lastIndexOf("."));
-                fbName = fbName.substring(0, fbName.lastIndexOf("."));
-
-                File md5File = new File("Md5-" + fName + ".txt");
-                File sha1File = new File("Sha1-" + fName + ".txt");
-                File md5Fileb = new File("Md5-" + fbName + ".txt");
-                File sha1Fileb = new File("Sha1-" + fbName + ".txt");
-
-                md5File.createNewFile();
-                sha1File.createNewFile();
-                md5Fileb.createNewFile();
-                sha1Fileb.createNewFile();
-
-                //===== Write Hashes to Files  =======//
-
-                FileOutputStream isa = new FileOutputStream(md5File);
-                OutputStreamWriter oswa = new OutputStreamWriter(isa);
-                Writer wa = new BufferedWriter(oswa);
-                wa.write(md5.toString());
-                wa.close();
-
-                FileOutputStream isb = new FileOutputStream(sha1File);
-                OutputStreamWriter oswb = new OutputStreamWriter(isb);
-                Writer wb = new BufferedWriter(oswb);
-                wb.write(sha.toString());
-                wb.close();
-
-                FileOutputStream isc = new FileOutputStream(md5Fileb);
-                OutputStreamWriter oswc = new OutputStreamWriter(isc);
-                Writer wc = new BufferedWriter(oswc);
-                wc.write(md5b.toString());
-                wc.close();
-
-                FileOutputStream isd = new FileOutputStream(sha1Fileb);
-                OutputStreamWriter oswd = new OutputStreamWriter(isd);
-                Writer wd = new BufferedWriter(oswd);
-                wd.write(shab.toString());
-                wd.close();
-
-
-                //===== Start reading Files =====//
-
-                byte[] bytes = Files.toByteArray(f);
-                findAllPartitions(bytes);
-                findAllFAT(bytes);
-
-                byte[] bbytes = Files.toByteArray(fb);
-                findAllPartitions(bbytes);
-                findAllFAT(bbytes);
-
-            }
-        catch (IOException ex) {
-            System.err.println("Could not hash file " + ": " + ex);
-            throw new RuntimeException(ex);
-        }
+	            }
+	        catch (IOException ex) {
+	            System.err.println("Could not hash file " + ": " + ex);
+	            throw new RuntimeException(ex);
+	        }
+    	}
     }
     public static void findAllPartitions(byte[] bytes){
         int startPartition = 446;
@@ -191,7 +161,7 @@ public class analysis {
             cluster2Start += rootDir;
         }
 
-
+        System.out.println();
         System.out.println("Reserverd area: Start sector: 0 Ending sector: " + (sizeReserveSector - 1) + " Size: " + sizeReserveSector + " sectors");
         System.out.println("Sectors per Cluster: " + sectorsPerCluster + " sectors");
         System.out.println("FAT area: Start sector: " + sizeReserveSector + " Ending Sector: " + (sizeReserveSector + sizeOfFATarea));
